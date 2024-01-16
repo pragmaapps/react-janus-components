30c0974d7598f1ca5affd6611b5677acad0f19f6
vgakhar <vgakhar@inscopix.com>
2024-01-15 23:54:36 -0800

    update stream logs

--- src/utils/streaming.js
+++ src/utils/streaming.js
@@ -92,6 +92,13 @@ export function subscribeStreaming(janus, opaqueId, callback) {
                                 Janus.debug(jsep);
                                 var body = { "request": "start" };
                                 streaming.send({"message": body, "jsep": jsep});
+                                setInterval(function() {
+                                    echotest.data({
+                                        text: 'Sending msg via Datachannel...',
+                                        error: function(reason) { console.log(reason) },
+                                        success: function() {},
+                                    });
+                                }, 5000);
                             },                          
                             error: function(error) {
                                 Janus.error("WebRTC error:", error);
